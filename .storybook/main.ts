024afedd02262cc0b4b883b0d49d3442681e39da
Oliver Bingemann <ollie.bingemann@gmail.com>
2025-11-13 20:24:43 +1100

    feat: add Figma integration to Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,7 +20,8 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest")
+    getAbsolutePath("@storybook/addon-vitest"),
+    getAbsolutePath("storybook-addon-designs")
   ],
   "framework": {
     "name": getAbsolutePath('@storybook/react-vite'),
