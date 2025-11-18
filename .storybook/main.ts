3dc7e65c2c69d0682df419588752d4507590d6bb
Oliver Bingemann <ollie.bingemann@gmail.com>
2025-11-18 13:39:03 +1100

    Move all story files to /stories folder
    - Moved 11 design system story files from packages to /stories
    - Updated Storybook config to only use /stories
    - Cleaner organization, stories separate from components
    
    All stories now in one location ✅

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,8 +13,7 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   "stories": [
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     getAbsolutePath('@chromatic-com/storybook'),
